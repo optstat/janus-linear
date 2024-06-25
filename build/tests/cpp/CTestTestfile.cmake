78bed69f98ab66f030fc818b4b91633887cac16b
Panos Lambrianides <panos@protonomics.com>
2024-06-25 18:31:41 +0000

    Remove build directory from repository

--- build/tests/cpp/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/panos/Applications/research/janus-linear/tests/cpp
-# Build directory: /home/panos/Applications/research/janus-linear/build/tests/cpp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LinearTest "/home/panos/Applications/research/janus-linear/build/bin/lineartest")
-set_tests_properties(LinearTest PROPERTIES  _BACKTRACE_TRIPLES "/home/panos/Applications/research/janus-linear/tests/cpp/CMakeLists.txt;19;add_test;/home/panos/Applications/research/janus-linear/tests/cpp/CMakeLists.txt;0;")
